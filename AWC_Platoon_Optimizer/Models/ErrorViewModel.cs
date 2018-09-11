aa98c1d4ad07e3621ba1f643f275d55c0cacb398
Anthony Vatter <anthony.vatter@morleynet.com>
2018-09-10 21:09:07 -0400

    00001-Initial Commit

---
+++ AWC_Platoon_Optimizer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AWC_Platoon_Optimizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
